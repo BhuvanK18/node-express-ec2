6cca5d5e04fb2946aabb63dda1c105211721f7f2
Bhuvan Kohli <bhuvankohli16@gmail.com>
2023-02-11 15:42:42 +0530

    Update conn.js

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,6 +1,6 @@
 // Simply connecting the mongodb server to our project using Mongoose
 const mongoose = require("mongoose");
-mongoose.connect("mongodb+srv://Bhuvan123:Bhuvan123@cluster0.ggrfg.mongodb.net/?retryWrites=true&w=majority", {
+mongoose.connect("mongodb://localhost:27017/recipeHub", {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => {
